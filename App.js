84fe0ea953e6adf156d3ecdcb0cd8b7eaa66ad28
Hardik Jade <hardikjade2808@gmail.com>
2022-03-05 23:51:33 +0530

    Bug Fixing

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 require('dotenv').config()
 const express = require('express');
-const cors = require('cors')
 const connectDatabase = require('./db_connect');
 const port = process.env.BACKEND_PORT
-const mongoose = require('mongoose');
-const { request } = require('express');
-const conn = mongoose.connection;
 const app = express()
-// app.use(cors());
 app.use(express.json())
 connectDatabase().then(()=>{
     app.get('/',(request,response)=>{response.sendFile(__dirname + '/temp.html')})
